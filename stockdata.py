b058772038fcf6b3b8e972ade5e9ab6339c91366
Kropium <kyli_780@hotmail.com>
2024-11-14 14:45:52 +0100

    Add files via upload

---
+++ stockdata.py
@@ -0,0 +1,60 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+
+
+class Stock:
+    def __init__(self, symbol, date, open_price, close_price, volume,):
+        self.symbol = symbol
+        self.date = date
+        self.open_price = open_price
+        self.close_price = close_price
+        self.volume = volume
+
+    def __repr__(self):
+        return f"Stock({self.symbol}, {self.date}, {self.open_price}, {self.close_price}, {self.volume})"
+
+current_time = datetime.now()
+current_time = current_time.replace(microsecond=0)
+time_minus10 = current_time - timedelta(minutes=3370)
+
+# Define the stock symbol and time range
+symbol = 'GME' 
+start_date = time_minus10
+end_date = current_time
+
+data = yf.download(symbol, start=start_date, end=end_date, interval='1m')
+
+stocks = []
+
+for date, row in data.iterrows():
+    stock = Stock(
+        symbol='AAPL',
+        date=date,
+        open_price=row['Open'],
+        close_price=row['Close'],
+        volume=row['Volume']
+    )
+    stocks.append(stock)
+
+print(stock)
+
+#class StockMarketChecker:
+    #def __init__(self, stocks):
+        #self.stocks = stocks  # List of Stock objects 
+
+   # def loop_until_desired_stock(self):
+        #condition_met = False
+        #attempts = 0 
+
+#while not condition_met and attempts < len(self.stocks):  
+
+#if self.check_conditions(stock):
+# Do something here, create function to notify
+#condition_met = True
+#else:
+# continue searching in list of stocks
+#attempts += 1
+
+#def check_conditions(self, stock):
+    #return stock.price > 100 and stock.volume > 1000 and stock.trend == "up"
