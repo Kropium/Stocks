4a4177eeeeaae31276c8611129d77634f1c9f94a
Kropium <kyli_780@hotmail.com>
2024-11-14 15:17:36 +0100

    Update stockdata.py

--- stockdata.py
+++ stockdata.py
@@ -4,19 +4,24 @@ from datetime import datetime, timedelta
 
 
 class Stock:
-    def __init__(self, symbol, date, open_price, close_price, volume,):
+    def __init__(self, symbol, timestamp, open_price, close_price, high, low, volume):
         self.symbol = symbol
-        self.date = date
+        self.timestamp = pd.to_datetime(timestamp)
         self.open_price = open_price
         self.close_price = close_price
+        self.high = high
+        self.low = low
         self.volume = volume
 
-    def __repr__(self):
-        return f"Stock({self.symbol}, {self.date}, {self.open_price}, {self.close_price}, {self.volume})"
+def __repr__(self):
+        # Return a string with all the values for easy reading
+        return (f"Stock(symbol={self.symbol}, timestamp={self.timestamp}, "
+                f"Open={self.open_price}, Close={self.close_price}, "
+                f"High={self.high}, Low={self.low}, Volume={self.volume})")
 
 current_time = datetime.now()
 current_time = current_time.replace(microsecond=0)
-time_minus10 = current_time - timedelta(minutes=3370)
+time_minus10 = current_time - timedelta(minutes=370)
 
 # Define the stock symbol and time range
 symbol = 'GME' 
@@ -25,20 +30,24 @@ end_date = current_time
 
 data = yf.download(symbol, start=start_date, end=end_date, interval='1m')
 
+print(data.head())
+
 stocks = []
 
-for date, row in data.iterrows():
+for timestamp, row in data.iterrows():
     stock = Stock(
-        symbol='AAPL',
-        date=date,
+        symbol='GME',
+        timestamp=timestamp,  
         open_price=row['Open'],
         close_price=row['Close'],
+        high=row['High'],
+        low=row['Low'],
         volume=row['Volume']
     )
     stocks.append(stock)
 
-print(stock)
-
+for stock in stocks[:10]:
+    print(f"volume: {stock.volume}")
 #class StockMarketChecker:
     #def __init__(self, stocks):
         #self.stocks = stocks  # List of Stock objects 
